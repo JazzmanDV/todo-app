d4220b5dc5eebdb965d4b6049e6773819b8b94ec
Dan <danapple2013@gmail.com>
2022-02-13 16:05:39 +0300

    TodoItem изменен с класса на функцию

--- src/components/TodoItem/TodoItem.jsx
+++ src/components/TodoItem/TodoItem.jsx
@@ -1,20 +1,17 @@
-import React from "react";
+import React, { useState, createRef, useEffect, useLayoutEffect } from "react";
 
 import styles from "./TodoItem.module.css";
 
 import { ReactComponent as DeleteIcon } from "./delete-icon.svg";
 
-export default class TodoItem extends React.Component {
-    willBeDeleted = false;
+const TodoItem = (props) => {
+    let willBeDeleted = false;
 
-    listItemRef = React.createRef();
+    const listItemRef = createRef();
+    const [visible, setVisible] = useState(props.visible);
 
-    state = {
-        visible: true,
-    };
-
-    show = () => {
-        const listItem = this.listItemRef.current;
+    const show = () => {
+        const listItem = listItemRef.current;
 
         const listItemHeight = getComputedStyle(listItem).height;
 
@@ -24,8 +21,8 @@ export default class TodoItem extends React.Component {
         });
     };
 
-    hide = (deleteAfterHide = false) => {
-        const listItem = this.listItemRef.current;
+    const hide = (deleteAfterHide = false) => {
+        const listItem = listItemRef.current;
 
         listItem.style.border = 0;
         listItem.style.height = getComputedStyle(listItem).height;
@@ -34,75 +31,69 @@ export default class TodoItem extends React.Component {
         });
 
         if (deleteAfterHide) {
-            listItem.addEventListener("transitionend", this.handleTransitionEnd);
+            addTransitionEndListener(listItem, "height", () => props.onDelete(props.index));
         }
     };
 
-    handleCheckboxChange = (e) => {
-        this.props.onIsDoneChange(this.props.index);
+    const handleCheckboxChange = (e) => {
+        props.onIsDoneChange(props.index);
     };
 
-    handleTransitionEnd = (e) => {
-        if (e.propertyName === "height") {
-            this.props.onDelete(this.props.index);
-        }
+    const addTransitionEndListener = (target, property, callback) => {
+        const eventListenerCallback = (e) => {
+            if (e.propertyName === property) {
+                callback();
+                e.target.removeEventListener("transitionend", eventListenerCallback);
+            }
+        };
+        target.addEventListener("transitionend", eventListenerCallback);
     };
 
-    handleButtonClick = (e) => {
-        if (this.willBeDeleted) {
+    const handleDeleteButtonClick = (e) => {
+        if (willBeDeleted) {
             return;
         }
 
-        this.willBeDeleted = true;
+        willBeDeleted = true;
 
-        this.hide(true);
+        hide(true);
     };
 
-    componentDidMount() {
-        if (this.props.visible) {
-            this.show();
+    useEffect(() => {
+        if (!visible && props.visible) {
+            setVisible(true);
         }
-        if (!this.props.visible) {
-            this.setState({ visible: false });
+        if (visible && !props.visible) {
+            hide();
+            addTransitionEndListener(listItemRef.current, "height", () => setVisible(false));
         }
-    }
+    }, [props.visible]);
 
-    componentDidUpdate(prevProps) {
-        if (prevProps.visible !== this.props.visible) {
-            if (this.props.visible) {
-                this.setState({ visible: true }, () => this.show());
-            }
-            if (!this.props.visible) {
-                this.hide();
-                this.listItemRef.current.addEventListener("transitionend", () => this.setState({ visible: false }));
-            }
+    useLayoutEffect(() => {
+        if (visible) {
+            show();
         }
-    }
+    }, [visible]);
 
-    componentWillUnmount() {
-        this.listItemRef.current.removeEventListener("transitionend", this.handleTransitionEnd);
-    }
+    const todo = props.todo;
+    const spanClassNames = [styles.text, todo.isDone ? styles.done : ""].join(" ");
 
-    render() {
-        const todo = this.props.todo;
+    return visible ? (
+        <li ref={listItemRef} className={styles["list-item"]}>
+            <label className={styles["list-item__inner"]}>
+                <input
+                    type="checkbox"
+                    checked={todo.isDone}
+                    onChange={handleCheckboxChange}
+                    className={styles.checkbox}
+                ></input>
+                <span className={spanClassNames}>{todo.text}</span>
+                <button onClick={handleDeleteButtonClick} className={`button ${styles["delete-button"]}`}>
+                    <DeleteIcon className={styles["delete-icon"]} />
+                </button>
+            </label>
+        </li>
+    ) : null;
+};
 
-        const spanClassNames = [styles.text, todo.isDone ? styles.done : ""].join(" ");
-
-        return this.state.visible ? (
-            <li ref={this.listItemRef} className={styles["list-item"]}>
-                <label className={styles["list-item__inner"]}>
-                    <input
-                        type="checkbox"
-                        checked={todo.isDone}
-                        onChange={this.handleCheckboxChange}
-                        className={styles.checkbox}
-                    ></input>
-                    <span className={spanClassNames}>{todo.text}</span>
-                    <button onClick={this.handleButtonClick} className={`button ${styles["delete-button"]}`}>
-                        <DeleteIcon className={styles["delete-icon"]} />
-                    </button>
-                </label>
-            </li>
-        ) : null;
-    }
-}
+export default TodoItem;
