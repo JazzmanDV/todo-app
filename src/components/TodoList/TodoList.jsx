8178fa9c19cb69704b2dbcdd5d61f7ae8ef75816
Dan <danapple2013@gmail.com>
2022-02-13 14:20:58 +0300

    TodoList изменен с класса на функцию

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -4,38 +4,38 @@ import TodoItem from "../TodoItem/TodoItem";
 
 import styles from "./TodoList.module.css";
 
-export default class TodoList extends React.Component {
-    render() {
-        const todos = this.props.todos;
-        const filter = this.props.filter;
+const TodoList = (props) => {
+    const todos = props.todos;
+    const filter = props.filter;
 
-        const todoList = todos.map((todo, index) => {
-            let visible = false;
-            if (filter === "all") {
-                visible = true;
-            }
-            if (filter === "done" && todo.isDone) {
-                visible = true;
-            }
-            if (filter === "not done" && !todo.isDone) {
-                visible = true;
-            }
-            return (
-                <TodoItem
-                    key={todo.id}
-                    todo={todo}
-                    index={index}
-                    visible={visible}
-                    onIsDoneChange={this.props.onIsDoneChange}
-                    onDelete={this.props.onDelete}
-                />
-            );
-        });
-
+    const todoList = todos.map((todo, index) => {
+        let visible = false;
+        if (filter === "all") {
+            visible = true;
+        }
+        if (filter === "done" && todo.isDone) {
+            visible = true;
+        }
+        if (filter === "not done" && !todo.isDone) {
+            visible = true;
+        }
         return (
-            <div className={styles["todo-list"]}>
-                <ul className={styles.list}>{todoList}</ul>
-            </div>
+            <TodoItem
+                key={todo.id}
+                todo={todo}
+                index={index}
+                visible={visible}
+                onIsDoneChange={props.onIsDoneChange}
+                onDelete={props.onDelete}
+            />
         );
-    }
-}
+    });
+
+    return (
+        <div className={styles["todo-list"]}>
+            <ul className={styles.list}>{todoList}</ul>
+        </div>
+    );
+};
+
+export default TodoList;
